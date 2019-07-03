615a53a69fe76d0b234ae2f659bf43ea1abea510
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-07-02 20:52:36 -0400

    [Neeraj] Adding navigation

---
+++ Day3ImageAndMore/ScreenThreeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ScreenThreeViewController.swift
+//  Day3ImageAndMore
+//
+//  Created by Neeraj Prasher on 2019-07-02.
+//  Copyright © 2019 njprasher. All rights reserved.
+//
+
+import UIKit
+
+class ScreenThreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //hiding back button
+        navigationItem.hidesBackButton = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
